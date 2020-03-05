ecc6e12e8af0b368d052adf170405c602e921de7
Andy Bradbury <andy@thestartupguys.co.uk>
2020-03-05 14:10:23 +0000

    upload initial files

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+
+const app = express()
+app.use(express.json())
+
+let db, trips, expenses
+
+mongo.connect(
+  url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db('tripcost')
+    trips = db.collection('trips')
+    expenses = db.collection('expenses')
+  }
+)
+
+app.post('/trip', (req, res) => {
+  const name = req.body.name
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ ok: true })
+  })
+})
+
+app.get('/trips', (req, res) => {
+  trips.find().toArray((err, items) => {
+      if(err) {
+        console.log(error)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ trips: items})
+  })
+})
+app.post('/expense', (req, res) => {
+  expenses.insertOne(
+  {
+    trip: req.body.trip,
+    date: req.body.date,
+    amount: req.body.amount,
+    category: req.body.category,
+    description: req.body.description
+  },
+  (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ ok: true })
+  }
+  )
+  })
+app.get('/expenses', (req, res) => {
+  /* */
+})
+
+app.listen(3000, () => console.log('Server ready'))
